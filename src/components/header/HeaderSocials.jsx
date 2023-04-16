6afd8f8e840a8f905ca74101c1c17da56def3e7c
mrrolbot23 <roldandelgado@icloud.com>
2023-04-16 08:38:23 -0400

    update crypto tracker links and GH link

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import {BsLinkedin, BsGithub} from 'react-icons/bs';
-import {FaCodepen} from 'react-icons/fa';
+import React from "react";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { FaCodepen } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/edwin-roldan-delgado-93b067b3/" target="_blank" rel="noreferrer" aria-label='linkedin button'><BsLinkedin/></a>
-        <a href="https://github.com/MrRolbot" target="_blank" rel="noreferrer" aria-label='github button'><BsGithub/></a>
-        <a href="https://codepen.io/mrrolbot" target="_blank" rel="noreferrer" aria-label='codepen button'><FaCodepen/></a>
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/edwin-roldan-delgado-93b067b3/"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="linkedin button"
+      >
+        <BsLinkedin />
+      </a>
+      <a
+        href="https://github.com/MrRolbot23"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="github button"
+      >
+        <BsGithub />
+      </a>
+      <a
+        href="https://codepen.io/mrrolbot"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="codepen button"
+      >
+        <FaCodepen />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,77 +1,131 @@
-import React from 'react';
-import './Portfolio.css';
-import IMG1 from '../../assets/portfolio1.webp'
-import IMG2 from '../../assets/portfolio2.webp'
-import IMG3 from '../../assets/pumptoshi 3D.webp'
-import IMG4 from '../../assets/crypto-tracker.webp'
+import React from "react";
+import "./Portfolio.css";
+import IMG1 from "../../assets/portfolio1.webp";
+import IMG2 from "../../assets/portfolio2.webp";
+import IMG3 from "../../assets/pumptoshi 3D.webp";
+import IMG4 from "../../assets/crypto-tracker.webp";
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>
+    <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Projects</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
+        <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>Student Management App</h3>
           <p>
-            The frontend was built with REACT, Ant Design, and NodeJS. 
-            The backend was build with Spring Boot, Maven, and PostgreSQL. Packaged 
-            with Docker into a single image. The app was deployed on <span className='highlight-bold' >AWS</span> through Github Actions
+            The frontend was built with REACT, Ant Design, and NodeJS. The
+            backend was build with Spring Boot, Maven, and PostgreSQL. Packaged
+            with Docker into a single image. The app was deployed on{" "}
+            <span className="highlight-bold">AWS</span> through Github Actions
             following CICD principals.
           </p>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/MrRolbot/student-app-springboot-react" className='btn' target='_blank' rel="noreferrer" >Github</a>
-              <a href="http://studentappspringbootreact-env.eba-tfkg7y2b.us-west-2.elasticbeanstalk.com/" className='btn btn-primary' target='_blank' rel="noreferrer" >Live Demo</a>
-            </div>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/MrRolbot/student-app-springboot-react"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="http://studentappspringbootreact-env.eba-tfkg7y2b.us-west-2.elasticbeanstalk.com/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
-        <article className='portfolio__item'>
+        <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG4} alt="" />
           </div>
           <h3>Cryptocurrency Tracker</h3>
-          <p> 
-            Built with REACT, NodeJS, Express, and the CoinGecko API. Easily track
-            your favorite coin's price, trending coins, and exchanges. Currently Hosted on <span className='highlight-bold' >Heroku</span>.
+          <p>
+            Built with REACT, NodeJS, Express, and the CoinGecko API. Easily
+            track your favorite coin's price, trending coins, and exchanges.
+            Currently Hosted on{" "}
+            <span className="highlight-bold">
+              <strong>Render</strong>
+            </span>
+            .
           </p>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/MrRolbot/Crypto-React-App" className='btn' target='_blank' rel="noreferrer" >Github</a>
-              <a href="https://cryptocurrency-react-app.herokuapp.com/" className='btn btn-primary' target='_blank' rel="noreferrer" >Live Demo</a>
-            </div>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/MrRolbot23/crypto-tracker"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://coin-tracker-0e2n.onrender.com/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
-        <article className='portfolio__item'>
+        <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG2} alt="" />
           </div>
           <h3>Newsletter Signup</h3>
           <p>
-            This application was built with
-            HTML, CSS, Bootsrap, NodeJS, and the MailChimp API. It 
-            can easily be implemented on any site. Deployed on <span className='highlight-bold' >Heroku </span>  
+            This application was built with HTML, CSS, Bootsrap, NodeJS, and the
+            MailChimp API. It can easily be implemented on any site. Deployed on{" "}
+            <span className="highlight-bold">Heroku </span>
             utilizing Git.
           </p>
-            <div className="portfolio__item-cta">
-              <a href="https://github.com/MrRolbot/Newsletter" className='btn' target='_blank' rel="noreferrer" >Github</a>
-              <a href="https://vast-lowlands-53275.herokuapp.com/" className='btn btn-primary' target='_blank' rel="noreferrer" >Live Demo</a>
-            </div>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/MrRolbot/Newsletter"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://vast-lowlands-53275.herokuapp.com/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
-        <article className='portfolio__item'>
+        <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG3} alt="" />
           </div>
           <h3>Pumptoshi Website (In Progress)</h3>
-            <p>Cryptopreneur and Financial mentor. Built with REACT and NodeJS.</p>
-            <div className="portfolio__item-cta">
-              <a href="null" className='btn' rel="noreferrer" >Github</a>
-              <a href="null" className='btn btn-primary'rel="noreferrer">Live Demo</a>
-            </div>
+          <p>
+            Cryptopreneur and Financial mentor. Built with REACT and NodeJS.
+          </p>
+          <div className="portfolio__item-cta">
+            <a href="null" className="btn" rel="noreferrer">
+              Github
+            </a>
+            <a href="null" className="btn btn-primary" rel="noreferrer">
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
